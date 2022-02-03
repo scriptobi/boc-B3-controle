1ad3b69a0689c8f0ddac9583a80ff90c7bca344e
c5819 <xx456456>
2022-02-03 21:06:29 +0100

    commit initial

---
+++ src/test/java/com/brestopencampus/b3/ic/controle/ControleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brestopencampus.b3.ic.controle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
